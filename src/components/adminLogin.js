c7dfb186de8a277c0b0e6d0c450bb06f88f29f09
aj-marci <abmarcinek@gmail.com>
2023-08-25 13:27:38 -0500

    added client side validation with formik and yup, removed server-side validation alert, adjusted styling

--- src/components/adminLogin.js
+++ src/components/adminLogin.js
@@ -2,16 +2,102 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../index.js";
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Formik } from "formik";
+import * as Yup from 'yup';
 
 const Login = () => {
-  const [email, setEmail] = useState({
-    value: "",
-    isTouched: false,
-  });
-const [password, setPassword] = useState("");
 
-const onLogin = (e) => {
-  e.preventDefault();
+  const adminSchema = Yup.object().shape({
+    email: Yup.string().matches(/abmarcinek@gmail.com/, "Not an admin, sorry! Contact for permission.")
+    .required("Required"),
+    password: Yup.string().required("Required"),
+  });
+
+  return (
+        <>
+        <div className="bg-background h-screen
+              font-gotham text-orange px-12 py-12">
+        <Formik   initialValues={{
+                    email: "",
+                    password: ""}}
+                  validationSchema={adminSchema}
+                  onSubmit={async (values) => {
+                    signInWithEmailAndPassword(auth, values.email, values.password)
+                    .then((userCredential) => {
+                        // Signed in
+                        const user = userCredential.user;
+                        localStorage.setItem('AJadminEmail', values.email);
+                        window.location.reload();
+                    })
+                    .catch((error) => {
+                        const errorCode = error.code;
+                        const errorMessage = error.message;
+                        console.log(errorCode, errorMessage)
+                    });
+                }}
+          >
+                  {({
+                  values,
+                  errors,
+                  handleChange,
+                  handleSubmit,
+                  dirty,
+       }) => (
+        <form
+        noValidate
+        onSubmit={handleSubmit}
+        className="font-gotham">
+                <div className="flex flex-col mb-2">
+                    <label className="text-base mb-1
+                    lg:text-base md:text-base text-sm text-darkcream">Admin Email</label>
+                    <input
+                    value={values.email}
+                    onChange={handleChange}
+                    aria-label="enter your email"
+                    name="email"
+                    className="lg:text-base md:text-base text-sm
+                    px-2 py-1 rounded bg-groupHover
+                    border-2 border-turqoise focus:outline-none focus:border-4
+                    text-darkcream w-64"></input>
+                    <p className="font-gotham text-formError
+                        font-semibold text-sm">{errors.email}</p>
+                </div>
+                <div className="flex flex-col mb-2">
+                    <label className="text-base mb-1
+                    lg:text-base md:text-base text-sm text-darkcream">Admin Password</label>
+                    <input
+                    value={values.password}
+                    onChange={handleChange}
+                    aria-label="enter password"
+                    name="password"
+                    className="lg:text-base md:text-base text-sm
+                    px-2 py-1 rounded bg-groupHover border-2 border-turqoise
+                    focus:outline-none focus:border-4 text-darkcream w-64"
+                    type="password"></input>
+                    <p className="font-gotham text-formError
+                        font-semibold text-sm">{errors.password}</p>
+                </div>
+                <button
+                type="submit"
+                disabled={!dirty || errors.email || errors.password}
+                className="text-base hover:text-turqoise
+                            font-semibold mb-4">Login</button>
+                <div>
+                <Link to="/"
+                className="font-gotham text-orange
+                hover:text-turqoise text-sm">Go To Home Page</Link>
+                </div>
+        </form>
+        )}
+        </Formik>
+        </div>
+        </>
+    )
+  }
+
+
+export default Login;
+/*  e.preventDefault();
   signInWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
       // Signed in
@@ -24,46 +110,5 @@ const onLogin = (e) => {
       const errorMessage = error.message;
       alert("Not an Admin, sorry! Contact for permission.")
       console.log(errorCode, errorMessage)
-  });
+  });*/
 
-}
-  return (
-        <>
-        <div className="flex flex-row justify-center bg-background h-screen
-              font-gotham text-orange py-24">
-        <form onSubmit={onLogin}>
-          <div>
-          <p>Email</p>
-          <input className="bg-background border-2 border-turqoise
-                focus:outline-none focus:border-4 rounded
-                mb-4 text-sm py-0.5 px-2"
-                  type="email"
-                  onChange={(e) => {
-                    setEmail(e.target.value)}}/>
-          </div>
-          <div>
-            <p>Password</p>
-            <input className="bg-background border-2 border-turqoise
-                focus:outline-none focus:border-4 rounded
-                mb-4 text-sm py-0.5 px-2"
-                  type="password"
-                  onChange={(e )=> {
-                    setPassword(e.target.value)}}/>
-          </div>
-          <div className="ml-11">
-          <button className="hover:text-turqoise rounded
-                border-2 border-orange px-4 py-2 hover:border-turqoise mb-4"
-                type="submit">Login</button>
-          </div>
-          <Link to="/"
-                className="font-gotham text-orange
-                hover:text-turqoise text-sm ml-6">Go To Home Page
-          </Link>
-        </form>
-        </div>
-        </>
-    )
-  }
-
-
-export default Login;
