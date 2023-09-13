4c185ec301f96487646213cf5eab45d82ac628a2
aj-marci <abmarcinek@gmail.com>
2023-09-13 15:20:27 -0500

    succesfully fetched and rendered contact form data, need to style

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,14 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Login from "./adminLogin";
-import db from "..";
+import { collection, getDocs } from "firebase/firestore";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import db from "..";
 
-const Admin = () => {
+function Admin() {
 
   const [formData, setFormData] = useState([]);
   const [loggedIn, setLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const auth = getAuth();
+
   onAuthStateChanged(auth, (user) => {
     if (user) {
       setLoggedIn(true);
@@ -26,49 +29,36 @@ const Admin = () => {
     });
   }
 
-      // Start the fetch operation as soon as
-    // the page loads
-    window.addEventListener('load', () => {
-      Fetchdata();
-  });
 
-  // Fetch the required data using the get() method
-  const Fetchdata = () => {
-      db.collection("messages").get().then((querySnapshot) => {
-
-          // Loop through the data and store
-          // it in array to display
-          querySnapshot.forEach(element => {
-              var data = element.data();
-              setFormData(arr => [...arr, data]);
-              console.log(data);
-
-          });
-      })
-  }
+  useEffect(()=>{
+    let mounted = true
+    getDocs(collection(db, "messages"))
+        .then((querySnapshot)=>{
+            const newData = querySnapshot.docs
+                .map((doc) => ({...doc.data(), id:doc.id }));
+            if (mounted) {
+            setLoading(false);
+            setFormData(newData);
+            console.log(formData);
+        }})
+        return () => mounted = false
+},[loading])
 // need to work on mapping contact form data
   return (
     <>
         { loggedIn ? (
         <div className="bg-background h-screen font-gotham">
         <div className="text-lg text-darkcream ml-4 mb-8 pt-8">Contact Form Submissions</div>
+        <div className="text-orange">
+        {
+        formData.map((formData,i)=>(
+            <p key={i} className="text-turqoise">
+                {formData.id}
+            </p>
+        ))
+    }
+        </div>
         <button onClick={handleLogout} className="text-orange">Logout</button>
-        <div className="bg-background lg:h-screen lg:pb-0 pb-8">
-        {formData.map(( messages ) => (
-        <div key={messages} className="grid grid-cols-3">
-        <div className="text-orange lg:col-span-1 lg:text-sm md:text-xs text-xs">
-            <p>{messages.message.values.email}</p>
-        </div>
-        <div className="text-lightcream lg:col-span-1 lg:text-lg font-semibold">
-            <p>{messages.message.values.name}</p>
-        </div>
-        <div className="text-lightcream lg:col-span-3 lg:text-base md:text-sm text-xs">
-        <p>{messages.message.values.message}</p>
-        <p>{messages.message.timestamp}</p>
-        </div>
-        </div>
-        ))}
-        </div>
         </div>
           ) : (
         <Login />
