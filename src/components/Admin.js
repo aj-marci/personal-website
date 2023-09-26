f9e05d7a67bd802e1e0e74dd7e8fa30c138ae0e9
aj-marci <abmarcinek@gmail.com>
2023-09-26 14:17:46 -0500

    added deleteDoc function to delete contact form submissions, added Font Awesome trash icon, styled submissions responsively

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
 import Login from "./adminLogin";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import db from "..";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 function Admin() {
 
   const [formData, setFormData] = useState([]);
   const [loggedIn, setLoggedIn] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [messageID, setMessageID] = useState([]);
 
   const auth = getAuth();
 
@@ -22,10 +25,8 @@ function Admin() {
 
   const handleLogout = () => {
     signOut(auth).then(() => {
-      // Sign-out successful.
       window.location.reload();
     }).catch((error) => {
-      // An error happened.
     });
   }
 
@@ -43,32 +44,54 @@ function Admin() {
         return () => mounted = false
 },[loading])
 
+
+const deleteMessage = async (id) => {
+  try {
+    if (window.confirm("Are you sure? This entry will be deleted forever.") == true)
+{   await deleteDoc(doc(db, "messages", id));
+    window.location.reload();
+  } else {
+    window.location.reload();
+  }
+  } catch (error) {
+    console.error("Error deleting document: ", error);
+  }
+}
+
   return (
     <>
         { loggedIn ? (
         <div className="bg-background h-screen font-gotham">
-        <div className="text-lg text-darkcream ml-4 mb-8 pt-8">Contact Form Submissions</div>
-        <div className="text-orange">
+        <div className="text-lg text-darkcream font-bold ml-4 mb-8 pt-8">
+          Contact Form Submissions
+        </div>
+        <div className="text-orange divide-y-2 divide-lightcream mx-4">
         {
-        formData.map((formData,i)=>(
-            <div key={i}
-              className="ml-4 hover:bg-groupHover
-                grid lg:grid-cols-5 md:grid-cols-5 sm:grid-cols-1 gap-4">
+        formData.map((formData)=>(
+            <div key={formData.id}
+              className="hover:bg-groupHover
+                grid lg:grid-cols-4 md:grid-cols-4 grid-cols-1 py-4
+                lg:my-0 md:my-0 my-4">
               <div className="lg:col-span-1">
                 {formData.message.values.name}
               </div>
               <div className="lg:col-span-1 text-turqoise">
                 {formData.message.values.email}
               </div>
-              <div className="lg:col-span-3 text-lightcream">
+              <div className="lg:col-span-1 text-lightcream">
                 "{formData.message.values.message}"
+              </div>
+              <div className="lg:col-span-1 text-formError">
+              <button onClick={() => deleteMessage(formData.id)}>
+                <FontAwesomeIcon icon={faTrash} size="lg"/>
+              </button>
               </div>
             </div>
         ))
     }
         </div>
         <button onClick={handleLogout}
-          className="text-orange ml-4 mt-8 hover:text-turqoise">Logout</button>
+          className="text-orange ml-4 mt-8 hover:text-turqoise font-bold">Logout</button>
         </div>
           ) : (
         <Login />
