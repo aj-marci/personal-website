06737beff1cab8b299af2a93b0ceb28bad452177
aj-marci <abmarcinek@gmail.com>
2023-11-29 10:53:32 -0600

    new language for nav and about section

--- src/components/About.js
+++ src/components/About.js
@@ -10,24 +10,18 @@ function About() {
         text-lightcream lg:text-base md:text-base text-xs max-w-md
         font-gotham leading-relaxed">
             <p className="lg:mb-2 md:mb-2 mb-1">In my senior year of college
-                                                I embarked on Co-founding
+                                                I embarked on co-founding
             <a href="https://www.endicott.edu/news-events/news/news-articles/2019/02/student-startup-living-proof-of-endicotts-entrepreneurial-spirit" target="_blank" rel="noopener noreferrer"
                 className="text-turqoise hover:text-orange font-semibold"> CropShop
             </a>,
             a B2B marketplace for Chefs and Local Farmers.
             </p>
             <p className="lg:mb-2 md:mb-2 mb-1">
-            For 4 years my focus was sales, marketing, operations,
+            Since co-founding CropShop, I've worked cross-functionally in sales, marketing, operations,
             <a href={Content}
                 target="_blank" rel="noopener noreferrer"
                 className="text-turqoise hover:text-orange font-semibold"> content creation
-            </a>, and UI / UX. In 2022
-            I
-            <a href={Certif}
-                target="_blank" rel="noopener noreferrer"
-                className="text-turqoise hover:text-orange font-semibold"> completed
-            </a> Meta's professional front-end
-            developer course.
+            </a>, frontend development, and UI / UX.
             </p>
             <p>
             When not at my computer, you can find me cooking, golfing, hiking,
@@ -35,7 +29,7 @@ function About() {
             <a href={AudRog}
                 target="_blank" rel="noopener noreferrer"
                 className="text-turqoise hover:text-orange font-semibold"> Audry
-            </a> and our Pup Roger.
+            </a> and our pup Roger.
             </p>
         </div>
         </>
