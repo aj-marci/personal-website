e3eb3ff6dc9feb4426c1a08e241a833f96e3fd6f
aj-marci <abmarcinek@gmail.com>
2024-02-07 10:43:54 -0600

    adjusted skills and career accordions to have more than one open at once and added hover styling

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,111 +5,101 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 function Experience() {
 
-  const [selectedSection, setSelectedSection] = useState("");
   const [educationVisible, setEducationVisible] = useState(false);
   const [workVisible, setWorkVisible] = useState(false);
   const [achievementsVisible, setAchievementsVisible] = useState(false);
 
-    const renderSection = (selectedSection) => {
-    if (selectedSection === "Education") {
-      return educationVisible && (
-        <ol className="relative border-l-2 border-turqoise mt-2">
-          <li className="mb-10 ml-4">
-            <div className="absolute w-4 h-4 -ml-6 rounded-full bg-turqoise"></div>
-            <p className="lg:text-sm md:text-sm text-xs italic">2022</p>
-            <p className="font-gotham font-semibold text-darkcream lg:text-base md:text-base text-xs">
-              Certified Professional Frontend Developer from Meta.
-            </p>
-          </li>
-          <li className="mb-10 ml-4">
-            <div className="absolute w-4 h-4 -ml-6 rounded-full bg-turqoise"></div>
-            <p className="lg:text-sm md:text-sm text-xs italic">2019</p>
-            <p className="font-gotham font-semibold text-darkcream lg:text-base md:text-base text-xs">
-              B.S. in Environmental Science from Endicott College.
-            </p>
-          </li>
-        </ol>
-      );
-    } else if (selectedSection === "Work") {
-      return workVisible && (
+  return (
+    <>
+    <div className="lg:mt-24 md:mt-20 mt-8 mb-2 font-gotham text-lightcream
+    lg:text-xl md:text-xl text-base font-semibold"
+    id="career-section">
+      <h1>Career</h1>
+    </div>
+      <div className='text-turqoise lg:text-lg md:text-lg text-base bg-groupHover
+        pl-4 rounded-md mb-4 py-2 hover:cursor-pointer hover:text-orange hover:font-semibold'
+        onClick={() => setEducationVisible(!educationVisible)}
+      >
+        Education
+      {educationVisible && (
         <ol className="relative border-l-2 border-turqoise mt-2">
         <li className="mb-10 ml-4">
           <div className="absolute w-4 h-4 -ml-6 rounded-full bg-turqoise"></div>
-          <p className="lg:text-sm md:text-sm text-xs italic">2022</p>
+          <p className="lg:text-sm md:text-sm text-xs italic text-darkcream font-normal">2022</p>
           <p className="font-gotham font-semibold text-darkcream lg:text-base md:text-base text-xs">
-            Software Engineer at Freelance
+            Certified Professional Frontend Developer from Meta.
           </p>
         </li>
         <li className="mb-10 ml-4">
           <div className="absolute w-4 h-4 -ml-6 rounded-full bg-turqoise"></div>
-          <p className="text-sm italic">2020</p>
+          <p className="lg:text-sm md:text-sm text-xs italic text-darkcream font-normal">2019</p>
           <p className="font-gotham font-semibold text-darkcream lg:text-base md:text-base text-xs">
-            Senior Experience Manager, Marketing Associate at Goby
-          </p>
-        </li>
-        <li className="mb-10 ml-4">
-          <div className="absolute w-4 h-4 -ml-6 rounded-full bg-turqoise"></div>
-          <p className="text-sm italic">2017</p>
-          <p className="font-gotham font-semibold text-darkcream lg:text-base md:text-base text-xs">
-            Co-founder & CEO at CropShop
+            B.S. in Environmental Science from Endicott College.
           </p>
         </li>
       </ol>
-      );
-    } else if (selectedSection === "Achievements") {
-            return achievementsVisible && (
+      )}
+    </div>
+    <div className='text-turqoise lg:text-lg md:text-lg text-base bg-groupHover
+        pl-4 rounded-md mb-4 py-2 hover:cursor-pointer hover:text-orange hover:font-semibold'
+        onClick={() => setWorkVisible(!workVisible)}>
+      Work
+      {workVisible && (
+            <ol className="relative border-l-2 border-turqoise mt-2">
+            <li className="mb-10 ml-4">
+            <div className="absolute w-4 h-4 -ml-6 rounded-full bg-turqoise"></div>
+              <p className="lg:text-sm md:text-sm text-xs italic text-darkcream font-normal">2022</p>
+             <p className="font-gotham font-semibold text-darkcream lg:text-base md:text-base text-xs">
+                  Software Engineer at Freelance
+              </p>
+              </li>
+              <li className="mb-10 ml-4">
+              <div className="absolute w-4 h-4 -ml-6 rounded-full bg-turqoise"></div>
+              <p className="lg:text-sm md:text-sm text-xs italic text-darkcream font-normal">2020</p>
+              <p className="font-gotham font-semibold text-darkcream lg:text-base md:text-base text-xs">
+                  Senior Experience Manager, Marketing Associate at Goby
+               </p>
+               </li>
+                <li className="mb-10 ml-4">
+                <div className="absolute w-4 h-4 -ml-6 rounded-full bg-turqoise"></div>
+                <p className="lg:text-sm md:text-sm text-xs italic text-darkcream font-normal">2017</p>
+                <p className="font-gotham font-semibold text-darkcream lg:text-base md:text-base text-xs">
+                  Co-founder & CEO at CropShop
+                </p>
+               </li>
+                </ol>
+           )}
+          </div>
+      <div className='text-turqoise lg:text-lg md:text-lg text-base bg-groupHover
+          pl-4 rounded-md mb-4 py-2 hover:cursor-pointer hover:text-orange hover:font-semibold'
+        onClick={() => setAchievementsVisible(!achievementsVisible)}>
+          Achievements
+            {achievementsVisible && (
               <ol className="relative border-l-2 border-turqoise mt-2">
               <li className="mb-10 ml-4">
                 <div className="absolute w-4 h-4 -ml-6 rounded-full bg-turqoise"></div>
-                <p className="text-sm italic">2018</p>
+                <p className="lg:text-sm md:text-sm text-xs italic text-darkcream font-normal">2018</p>
                 <p className="font-gotham font-semibold text-darkcream lg:text-base md:text-base text-xs">
                   1st Place Winner of Endicott College's Pitch Competition.
                 </p>
               </li>
               <li className="mb-10 ml-4">
                 <div className="absolute w-4 h-4 -ml-6 rounded-full bg-turqoise"></div>
-                <p className="text-sm italic">2015</p>
+                <p className="lg:text-sm md:text-sm text-xs italic text-darkcream font-normal">2015</p>
                 <p className="font-gotham font-semibold text-darkcream lg:text-base md:text-base text-xs">
                   Captain of Michigan Warriors Junior A Hockey Team.
                 </p>
               </li>
               <li className="mb-10 ml-4">
                 <div className="absolute w-4 h-4 -ml-6 rounded-full bg-turqoise"></div>
-                <p className="text-sm italic">2012</p>
+                <p className="lg:text-sm md:text-sm text-xs italic text-darkcream font-normal">2012</p>
                 <p className="font-gotham font-semibold text-darkcream lg:text-base md:text-base text-xs">
                   MI U-16 AAA Hockey State Champion.
                 </p>
               </li>
             </ol>
-      );
-    }
-  };
-
-    return (
-    <>
-     <div className="lg:mt-24 md:mt-20 mt-8 mb-2 font-gotham text-lightcream
-                        lg:text-xl md:text-xl text-base font-semibold"
-                        id="career-section">
-        <h1>Career</h1>
-      </div>
-      <div className='text-turqoise lg:text-lg md:text-lg text-base
-                          bg-groupHover pl-4 rounded-md mb-4 py-2 hover:cursor-pointer'
-          onClick={() => {setSelectedSection("Education"); setEducationVisible(!educationVisible); }}>
-            Education
-            {selectedSection === "Education" ? renderSection(selectedSection): null }
-      </div>
-      <div className='text-turqoise lg:text-lg md:text-lg text-base
-                          bg-groupHover pl-4 rounded-md mb-4 py-2 hover:cursor-pointer'
-          onClick={() => {setSelectedSection("Work"); setWorkVisible(!workVisible); }}>
-            Work
-            {selectedSection === "Work" ? renderSection(selectedSection): null }
-      </div>
-      <div className='text-turqoise lg:text-lg md:text-lg text-base
-                          bg-groupHover pl-4 rounded-md mb-4 py-2 hover:cursor-pointer'
-          onClick={() => {setSelectedSection("Achievements"); setAchievementsVisible(!achievementsVisible); }}>
-            Achievements
-            {selectedSection === "Achievements" ? renderSection(selectedSection): null }
-      </div>
+            )}
+          </div>
       <div className="hover:translate-x-1 font-gotham font-semibold
                 text-turqoise hover:text-orange lg:mt-8 md:mt-8 mt-4">
              <a href={Resume}
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,85 +2,75 @@ import React, {useState} from 'react';
 
 function Experience() {
 
-  const [selectedSkills, setSelectedSkills] = useState("");
   // accordian state
   const [technicalVisible, setTechnicalVisible] = useState(false);
   const [growthVisible, setGrowthVisible] = useState(false);
   const [csVisible, setCsVisible] = useState(false);
 
 
-    const renderSection = (skills) => {
-    if (skills === "Technical") {
-      return technicalVisible && (
-        <div className='mt-2 lg:text-base md:text-base text-xs font-gotham font-semibold
-        grid grid-cols-3 gap-1 text-darkcream'>
-          <p>JavaScript</p>
-          <p>HTML</p>
-          <p>CSS</p>
-          <p>React.js</p>
-          <p>Express.js</p>
-          <p>Node.js</p>
-          <p>GCP</p>
-          <p>Heroku</p>
-          <p>MongoDB</p>
-          <p>REST</p>
-          <p>GitHub</p>
-          <p>Vercel</p>
-        </div>
-      );
-    } else if (skills === "Growth") {
-      return growthVisible && (
-        <div className='mt-2 lg:text-base md:text-base text-xs font-gotham font-semibold
-        grid grid-cols-3 gap-1 text-darkcream'>
+  return (
+    <>
+      <div className="lg:mt-24 md:mt-20 mt-8 mb-2 font-gotham text-lightcream lg:text-xl md:text-xl text-base font-semibold" id="skills-section">
+        <h1>Skills</h1>
+      </div>
+      <div
+        className='text-turqoise lg:text-lg md:text-lg text-base bg-groupHover pl-4
+            rounded-md mb-4 py-2 hover:cursor-pointer hover:text-orange hover:font-semibold'
+        onClick={() => setTechnicalVisible(!technicalVisible)}
+      >
+        Technical
+        {technicalVisible && (
+          <div className='mt-2 lg:text-base md:text-base text-xs font-gotham font-semibold grid grid-cols-3 gap-1 text-darkcream'>
+            <p>JavaScript</p>
+            <p>HTML</p>
+            <p>CSS</p>
+            <p>React.js</p>
+            <p>Express.js</p>
+            <p>Node.js</p>
+            <p>GCP</p>
+            <p>Heroku</p>
+            <p>MongoDB</p>
+            <p>REST</p>
+            <p>GitHub</p>
+            <p>Vercel</p>
+          </div>
+        )}
+      </div>
+      <div
+        className='text-turqoise lg:text-lg md:text-lg text-base bg-groupHover
+          pl-4 rounded-md mb-4 py-2 hover:cursor-pointer hover:text-orange hover:font-semibold'
+        onClick={() => setGrowthVisible(!growthVisible)}
+      >
+        Marketing
+        {growthVisible && (
+          <div className='mt-2 lg:text-base md:text-base text-xs font-gotham font-semibold grid grid-cols-3 gap-1 text-darkcream'>
             <p>Google Ads</p>
-            <p>Google Analytics</p>
             <p>Impact</p>
+            <p>Google Analytics</p>
             <p>Meta Ads</p>
             <p>Klaviyo</p>
             <p>Quora</p>
             <p>Affiliate Marketing</p>
-        </div>
-      );
-    } else if (skills === "CS") {
-        return csVisible && (
-          <div className='mt-2 lg:text-base md:text-base text-xs font-gotham font-semibold
-          grid grid-cols-3 gap-1 text-darkcream'>
+          </div>
+        )}
+      </div>
+      <div
+        className='text-turqoise lg:text-lg md:text-lg text-base bg-groupHover
+          pl-4 rounded-md mb-4 py-2 hover:cursor-pointer hover:text-orange hover:font-semibold'
+        onClick={() => setCsVisible(!csVisible)}
+      >
+        Customer Success
+        {csVisible && (
+          <div className='mt-2 lg:text-base md:text-base text-xs font-gotham font-semibold grid grid-cols-3 gap-1 text-darkcream'>
             <p>ZenDesk</p>
             <p>Salesforce</p>
             <p>SalesLoft</p>
             <p>Intercom</p>
-        </div>
-      );
-    }
-  };
-
-    return (
-    <>
-     <div className="lg:mt-24 md:mt-20 mt-8 mb-2 font-gotham text-lightcream
-                        lg:text-xl md:text-xl text-base font-semibold"
-                        id="skills-section">
-        <h1>Skills</h1>
-      </div>
-      <div className='text-turqoise lg:text-lg md:text-lg text-base
-                      bg-groupHover pl-4 rounded-md mb-4 py-2 hover:cursor-pointer'
-        onClick={() => {setSelectedSkills("Technical"); setTechnicalVisible(!technicalVisible); }}>
-          Technical
-        {selectedSkills === "Technical" ? renderSection(selectedSkills): null }
-      </div>
-      <div className='text-turqoise lg:text-lg md:text-lg text-base
-                          bg-groupHover pl-4 rounded-md mb-4 py-2 hover:cursor-pointer'
-          onClick={() => {setSelectedSkills("Growth"); setGrowthVisible(!growthVisible); }}>
-            Marketing
-            {selectedSkills === "Growth" ? renderSection(selectedSkills): null }
-      </div>
-      <div className='text-turqoise lg:text-lg md:text-lg text-base
-                          bg-groupHover pl-4 rounded-md mb-4 py-2 hover:cursor-pointer'
-          onClick={() => {setSelectedSkills("CS"); setCsVisible(!csVisible); }}>
-            Customer Success
-            {selectedSkills === "CS" ? renderSection(selectedSkills): null }
+          </div>
+        )}
       </div>
     </>
-    );
+  );
   }
 
   export default Experience;
